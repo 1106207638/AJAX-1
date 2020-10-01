102c3b49f15ef803bc9366aae1e2c6fc328a4222
1106207638 <1106207638@qq.com>
2020-10-01 20:31:32 +0800

    写完分页

---
+++ public/main.js
@@ -0,0 +1,100 @@
+const request = new XMLHttpRequest();
+getCSS.onclick = () => {
+  request.open("GET", "/style.css");
+  request.onreadystatechange = () => {
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
+        const style = document.createElement("style");
+        style.innerHTML = request.response;
+        document.head.appendChild(style);
+      } else {
+        alert("加载CSS失败");
+      }
+    }
+  };
+
+  request.send();
+};
+getJS.onclick = () => {
+  request.open("GET", "/2.js");
+  request.onreadystatechange = () => {
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
+        const script = document.createElement("script");
+        script.innerHTML = request.response;
+        document.body.appendChild(script);
+      } else {
+        alert("加载JS失败");
+      }
+    }
+  };
+
+  request.send();
+};
+getHTML.onclick = () => {
+  request.open("GET", "/3.html");
+  request.onreadystatechange = () => {
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
+        const div = document.createElement("div");
+        div.innerHTML = request.response;
+        document.body.appendChild(div);
+      } else {
+        alert("加载html失败");
+      }
+    }
+  };
+
+  request.send();
+};
+getXML.onclick = () => {
+  request.open("GET", "/4.xml");
+  request.onreadystatechange = () => {
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
+        const dom = request.responseXML;
+        const text = dom.getElementsByTagName("warning")[0].textContent;
+        console.log(text);
+      } else {
+        alert("加载xml失败");
+      }
+    }
+  };
+
+  request.send();
+};
+getJSON.onclick = () => {
+  request.open("GET", "/5.json");
+  request.onreadystatechange = () => {
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
+        const object = JSON.parse(request.response);
+        mingZi.textContent = object.name;
+      } else {
+        alert("加载html失败");
+      }
+    }
+  };
+
+  request.send();
+};
+let n = 1;
+getPage.onclick = () => {
+  request.open("GET", `/page${n + 1}`);
+  request.onreadystatechange = () => {
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
+        const array = JSON.parse(request.response);
+        array.forEach((item) => {
+          const li = document.createElement("li");
+          li.textContent = item.id;
+          xxx.appendChild(li);
+        });
+        n += 1;
+      } else {
+        alert("已经是最后一页了");
+      }
+    }
+  };
+  request.send();
+};
